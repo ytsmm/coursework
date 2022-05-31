77f7d99707ef6659161f05769d5e447d77405460
ytsmm <82078728+ytsmm@users.noreply.github.com>
2022-06-01 01:13:02 +0500

    Delete Tokenizer.py

--- code/Tokenizer.py
+++
@@ -1,6 +0,0 @@
-import nltk
-
-def Tokenizer(word_data):
-    token_text = nltk.word_tokenize(word_data)
-    # token_text - массив строковых данных, в котором хранятся токены статьи
-    return token_text
